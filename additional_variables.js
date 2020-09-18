e02ffdb5fc80abdb85c89683c6834a3fb96d937f
JulyPsy <71481959+JulyPsy@users.noreply.github.com>
2020-09-18 12:45:18 +0800

    Add files via upload

---
+++ additional_variables.js
@@ -0,0 +1,124 @@
+//intructions for Daw 2step task//
+//<meta charset="utf-8">
+var instructions_1a_text =  ["<div align=center>欢迎参加本实验！<br><br>请仔细阅读以下说明。<br><br>这会需要一点时间，但是请完全不用担心！<br><br>只要认真阅读就会清楚，在接下来的任务中你需要做些什么。</div>"]
+var instructions_1b_text = ["<div align=center><div style = 'width:700px'>在这个任务中, 你将选择一个投资顾问帮你投资基金，来获取利润。<br><br>这些基金来自两个金融机构，用不同颜色区分。<br><br><img style='margin:0px auto;display:block;height:200px' src='img/example_planets.png'/></div></div>"]
+var instructions_1c_text =["<div align=center>你可以选择两个投资顾问的一个,<br><br>他们会向你推荐不同的基金。<br><br><img style='margin:0px auto;display:block;height:400px' src='img/example_aliens.png'/><br></div></div>"]
+var instructions_1d_text =["<div align=center><div style = 'width:800px'><div align=justify>如果这个理财顾问眼光很好，所以如果他给你推荐的基金赚了钱，你就会得到这些收益。下面这个图片提示你赚到了钱:<br><br><img style='margin:0px auto;display:block' src='img/treasure.png'/><br>但是有时，理财顾问推荐给你投资的基金没有赚到钱，你就会看到一个空心的圆圈。<br><br><img style='margin:0px auto;display:block' src='img/noreward.png'/>为了方便你记忆，这里仅呈现“有收益”和“无收益”两种结果</div></div>"]
+var instructions_1e_text = ["<div align=justify><div style = 'width:800px'>如果这个理财顾问推荐的基金的市场走势不错，那么你就会因此获利。当然也不是每次投资你都会赚到钱，因为市场是会发生变动的。但是在绝大多数情况下，这会帮你带来收益。<br><br>另外一个理财顾问推荐的基金可能此时的市场走势不太好，所以，在现阶段你可能很难投资获利。<br><br>在每轮投资结束时，你所获得的收益将被转换为积分。<br><Br>在实验的最后，这些积分将会兑换成礼品。</div></div>"]
+var instructions_1f_text = ["<div align=justify><div style = 'width:700px'>你可以按 F 键选择左边的基金，按 J 键选择右边的基金。然后你就会知道这次的投资你有没有获得收益。<br><br>现在，可以试着练习几次。在接下来练习中，请始终选择被突出显示的那个基金。</div></div>"]
+//
+var instructions_2a_text = ["<div align=center><div style = 'width:800px'>你可能已经注意到了，这个基金的市场走势比较好。在大部分的时候，这个基金都会赚钱。<br><br>另外一个基金此时的市场走势可能没那么好。</div></div>"];
+//
+var instructions_3a_text = ["<div align=justify><div style = 'width:800px'> 发现了吗？理财顾问推荐你投资的这个基金可能不太好，能赚到的钱很少。<br><br>每个理财顾问都会向你推荐基金，但是也不是每次投资都能赚到钱的。某个基金可能更会帮你赚钱，因为这一时期该基金的市场走势比较好。<br><br>在接下来的练习阶段，你可以从两个基金中进行选择，看哪一个更能帮你赚钱。<br><br>每个基金有时候会出现在屏幕的左侧，有时候会出现在屏幕的右侧。<br><br>基金出现在哪一侧并不重要，不会出现比如说，左侧比右侧运气不好这样的情况。</div></div>"]
+//
+var instructions_4a_text = ["<div align=justify><div style = 'width:800px'>做得好! 你现在可能已经了解到理财顾问给你推荐的这个基金更有可能帮你赚钱：<br><br><img style='margin:0px auto;display:block;height:100px' src='img/purple_stim_2_happy.png'/><br>同样，即使这个基金在这一阶段能帮你赚更多的钱，但是你也不能确定这是否一直都帮你投资获利，因为市场是在不停变动的。<br><br>所以，理财顾问帮你投资的结果（基金是否获利）会发生改变。那些原本帮你赚到钱的基金可能会受到市场的影响而没有帮你盈利。同样，那些原本没有帮你赚到钱的基金也有可能随着市场的变化而使你获利。<br><br>基金的市场变化是非常缓慢的， 所以，一定要尽量集中精力去获得尽可能多的利益。</div></div>"]
+var instructions_4b_text = ["<div align=justify><div style = 'width:800px'>尽管基金的市场走势会随着时间而发生变化，但是，请注意：这些变化发生的非常缓慢。所以在一段时间内，之前获利的基金会持续获利。<br><br>想要在每个时刻都找到会市场走势好的、能赚钱的基金，你必须要集中精力。</div></div>"]
+var instructions_4c_text = ["<div align=justify><div style = 'width:800px'>现在，你知道如何选择基金了，你可以学习如何进行整个任务了。<br><br>有两家金融机构的基金，分别用不同的颜色的背景表示。<br><br><img style='margin:0px auto;display:block;height:200px' src='img/example_planets.png'</div></div>"]
+var instructions_4d_text = ["<div align=justify><div style = 'width:800px'>在每轮选择中，你首先要选择听从哪个理财顾问的建议。<br><br><img style='margin:0px auto;display:block;height:200px' src='img/example_rockets.png'/><br>理财顾问会给你推荐不同的基金，但是其中一个理财顾问主要给你推荐的是紫和金融的产品（紫色背景），另一个理财顾问主要推荐的则是虹麟金融的产品（红色背景）。<br><br>理财顾问推荐基金的偏好在任务中不会改变，也就是说，这个人很倾向于给你推荐某个金融机构的产品。<br><br>选择一个你认为能给你推荐基金收益最好的那个理财顾问，帮你赚取更多收益。但是，请注意：有的时候这个理财顾问也会向你推荐他另一家他不常推荐的机构的基金！</div></div>"]
+var instructions_4e_text = ["<div align=justify><div style = 'width:800px'>在完成整个任务之前，我们先来练习一下吧。在开始之前，这里有一些帮助你更好完成任务的一些小提示：<br><br>提示 #1:<br>记住哪些基金会给你带来收益。 基金的市场趋势的变化是会随着时间改变的。所以，现在有收益的基金可能之后就没有收益了。<br><br>提示 #2:<br>请记住，每个基金都有自己的市场变化趋势，不能简单认为一个基金的收益不好，那么另一个基金的收益就会好。<br>当你选择了理财顾问，他会推荐给你两个基金，你可以选择其中任何一个， 紧接着你就会看到这个基金是否能给你带来收益。<br>理财顾问没有欺骗你，所有基金都可能会帮你赚钱，只是他们会更多推荐与自己合作更多的金融机构的产品。 <br><br>提示 #3:<br>理财顾问的选择很重要，因为在某个时间段，某个基金收益情况要更好。找到那个会给你推荐收益更好基金的理财顾问，这样你就能获得更多收益。</div></div>"]
+function instructions_4f_text(){
+	return["<div align=justify><div style = 'width:800px'>在正式的任务中，你只有2秒的时间来做出每个选择。<br><br>也就是说，首先你有2秒的时间从两个理财顾问中选择其中一个，然后他们会给你推荐基金，同样你要在2秒内从两个基金中选择其中一个。<br><br>在刚开始的时候，你可能觉得没有时间做出选择，觉得任务困难。请不要急躁，随着时间推移，当你学会这个任务时，你会发现2秒足够做出选择。<br><br>我们建议，你要认真思考你的选择策略，但是也要相信你的直觉。通过集中注意力，你可以获得更多的积分，也就是说你会更多地通过基金赚钱。这其中有机会的原因，也需要你的选择技巧。<br><br>现在，你要进行"+nrpracticetrials+" 轮的练习。这些只是为了让你更加熟悉任务，因此也没有2秒选择的时间限制。</div></div>"]}
+//
+var instructions_5a_text = ["<div align=justify><div style = 'width:800px'>好的，你现在已经完成所有练习部分了。<br><br>在正式的任务中，你将面对两个理财顾问，并从他们推荐的两个基金中选择其中的一个。<br><br>请注意！接下来的理财顾问和基金的发展情况和练习阶段中的不一样！练习阶段中出现的情况只是为了让你更加了解该任务！因此，在之前的练习阶段中，基金的好坏是不重要的！重要的是接下来正式任务！<br><br>除此之外，其他方面都是相同的。<br><br>让我们来简单复习下任务：<br><br>请记住，你要通过投资基金获得尽可能多的收益。你要从两个理财顾问中挑选其中一个，每个理财顾问都会给你推荐基金，他们更喜欢推荐有合作关系的金融机构的基金，但是有时也会推荐其他的基金。你要从他们为你推荐的两个基金中选择其中一个。<br>随着时间的推移，这些基金的市场趋势会发生缓慢的变化，灵活地掌握哪些理财顾问会给你推荐更可能赚钱的基金，哪些基金会更能给你带来收益。</div></div>"]
+function instructions_5b_text(){
+	return ["<div align=justify><div style = 'width:800px'>理财顾问会一直在屏幕上呈现2秒，即使你做了决定也要等到时间结束后才会进入下一步。所以，你无法通过做出更快的决定来快速结束任务。每个人完成任务的时间都是一样的，所以，你要在有限的时间内做出更加有利于你的决定！<br><br>在每一轮投资获利都会获得一个积分，积分越多，在实验结束后，就可能兑换更好的奖品。<br><br>整个任务会有 "+nrtrials+" 轮投资，将会花费 35 分钟.<br><br>在任务中你可以休息。<br><br>在点击 '下一页' 之后，正式任务将会开始, 请确保你的两个食指分别放在'F' 和 'J' 键上！祝你好运!</div></div>"]}
+
+
+
+function createMemberInNormalDistribution(mean,std_dev){
+	return mean + (gaussRandom()*std_dev)
+}
+/*
+* Returns random number in normal distribution centering on 0.
+* ~95% of numbers returned should fall between -2 and 2
+*/
+function gaussRandom() {
+	var u = 2*Math.random()-1
+	var v = 2*Math.random()-1
+	var r = u*u + v*v
+
+	/*if outside interval [0,1] start over*/
+	if(r == 0 || r > 1) return gaussRandom()
+
+	var c = Math.sqrt(-2*Math.log(r)/r)
+	return u*c
+}
+
+function shuffle(o){
+	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x)
+	return o
+}
+
+var images = [
+	'img/earth_planet.png',
+	'img/earth_stim_1_deact.png',
+	'img/earth_stim_1.png',
+	'img/earth_stim_2_deact.png',
+	'img/earth_stim_2.png',
+	'img/example_aliens.png',
+	'img/example_planets.png',
+	'img/example_rockets.png',
+	'img/example_values_2.png',
+	'img/example_values.png',
+	'img/noreward.png',
+	'img/purple_planet.png',
+	'img/purple_stim_1_deact.png',
+	'img/purple_stim_1_happy.png',
+	'img/purple_stim_1_sad.png',
+	'img/purple_stim_1.png',
+	'img/purple_stim_2_deact.png',
+	'img/purple_stim_2_happy.png',
+	'img/purple_stim_2_sad.png',
+	'img/purple_stim_2.png',
+	'img/red_planet.png',
+	'img/red_stim_1_deact.png',
+	'img/red_stim_1_happy.png',
+	'img/red_stim_1_sad.png',
+	'img/red_stim_1.png',
+	'img/red_stim_2_deact.png',
+	'img/red_stim_2_happy.png',
+	'img/red_stim_2_sad.png',
+	'img/red_stim_2.png',
+	'img/treasure.png',
+	'img/5x.png',
+	'img/1x.png',
+]
+
+
+/* function that saves data in a mysql database */
+function save_data(data, table) {
+	var xhr = new XMLHttpRequest()
+	xhr.open('POST', 'write_data.php') // change 'write_data.php' to point to php script.
+	xhr.setRequestHeader('Content-Type', 'application/json')
+	xhr.onload = function() {
+		console.log(xhr.responseText)
+		if(xhr.status == 200){
+			var response = JSON.parse(xhr.responseText)
+			console.log(response.success)
+		}
+	}
+	xhr.send('data=' + data + '&table=' + table)
+}
+
+/* function that allows removal of a specific value */
+Array.prototype.remove = function() {
+	var what, a = arguments, L = a.length, ax
+	while (L && this.length) {
+		what = a[--L]
+		while ((ax = this.indexOf(what)) !== -1) {
+			this.splice(ax, 1)
+		}
+	}
+	return this
+}
+
+function getQueryVariable(variable)
+{
+	var query = window.location.search.substring(1)
+	var vars = query.split("&")
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=")
+		if(pair[0] == variable){return pair[1];}
+	}
+	return(false)
+}
